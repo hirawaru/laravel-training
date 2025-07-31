32832dc4e6aaad81a60d5029d7ceaba7fb8c9aec
hirawaru <leconghoainam04@gmail.com>
2025-07-31 13:35:57 +0700

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
-use App\Models\Task;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Middleware\CheckAdmin;
-use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Cách 1 :
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create')
-    ->middleware('admin');
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit')
-    ->middleware('admin');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//Cách 2:
-Route::resource('/tasks', TaskController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
